7d2270d14280d548d663466ec3161e8fd968cf7b
Ferenc- <ferenc.gm@gmail.com>
2016-11-19 19:49:20 +0100

    Resond in HTTP

--- server.cpp
+++ server.cpp
@@ -36,11 +36,37 @@ void session(socket_ptr socket) {
             }
             std::cout<< tag() << ": handled: "
                      << std::string(data, length) << std::endl;
+#if 1       // HTTP OK
+            const std::string response = {"HTTP/1.1 200 OK\r\n"
+                                          "Date: Mon, 23 May 2005 22:38:34 GMT\r\n"
+                                          "Content-Type: text/html; charset=UTF-8\r\n"
+                                          "Content-Encoding: UTF-8\r\n"
+                                          "Content-Length: 138\r\n"
+                                          "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n"
+                                          "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n"
+                                          "ETag: \"3f80f-1b6-3e1cb03b\"\r\n"
+                                          "Accept-Ranges: bytes\r\n"
+                                          "Connection: close\r\n"
+                                          "\r\n"
+                                          "<html>\r\n"
+                                          "<head>\r\n"
+                                          "  <title>An Example Page</title>\r\n"
+                                          "</head>\r\n"
+                                          "<body>\r\n"
+                                          "  Hello World, this is a very simple HTML document.\r\n"
+                                          "</body>\r\n"
+                                          "</html>\r\n"};
 
+            boost::asio::async_write(
+                    * socket,
+                    boost::asio::buffer(response),
+                    boost::fibers::asio::yield[ec]);
+#else       // ECHO mode
             boost::asio::async_write(
                     * socket,
                     boost::asio::buffer(data, length),
                     boost::fibers::asio::yield[ec]);
+#endif
 
             if (ec == boost::asio::error::eof) {
                 break; //connection closed cleanly by peer
